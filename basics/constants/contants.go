f00be813ca429ff816511df7bed988f0d090c384
JerinWilson <jerin.wilson@vonnue.com>
2025-05-24 14:34:29 +0530

    feat: constants

---
+++ basics/constants/contants.go
@@ -0,0 +1,19 @@
+package main
+
+const PI = 3.14
+const GRAVITY = 9.81
+
+func main() {
+
+	const days = 7
+
+	const (
+		MONDAY    = 1
+		TUESDAY   = 2
+		WEDNESDAY = 3
+		THURSDAY  = 4
+	)
+
+	name := "John Doe"
+
+}
